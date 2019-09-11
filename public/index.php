102eab863c9b739d402d1a99035dc39cb1f55211
Gustavo Fraga <gustavolafraga@gmail.com>
2019-09-11 12:08:53 -0300

    ajustes

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ $pdo = null;
 try {
     $pdo = new PDO('mysql:host=mysql;dbname=database', 'root', '12345');
 } catch (PDOException $e) {
-    echo $e->getMessage();
+    print $e->getMessage();
     die();
 }
 
